24ba96790c55b745f3296766929e8153d736dd31
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-03-14 10:58:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os 
-API_ID = 123456
-API_HASH = "your_api_hash_here"
-BOT_TOKEN = "your_bot_token_here"
-
+API_ID = 1234567
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
 OWNER_ID = 123456789
+
+STRING_SESSION = "your_string_session"
